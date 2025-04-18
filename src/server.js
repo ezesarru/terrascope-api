58ecb21a61763ec9d5d81ef6fa5c8a22b751b02d
EzeSarru <ezesarru@gmail.com>
2025-04-18 14:34:58 -0300

    fifth

--- src/server.js
+++ src/server.js
@@ -7,27 +7,22 @@ const helmet = require("helmet");
 require("dotenv").config();
 const { WEB_URL } = process.env;
 
+// CORS configuration
+const corsOptions = {
+  origin: WEB_URL,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+};
+
 server.use(helmet()); // HTTP receptor
 server.use(express.json()); // JSON reader
 server.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded data parser
 server.use(morgan("dev")); // Console request status
-server.use(
-  cors({
-    origin: WEB_URL,
-    credentials: true,
-  })
-);
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", WEB_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(cors(corsOptions));
+
 server.use(router);
+
 server.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong" });
